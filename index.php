60f0a15347d985a2c320c741817eb5a9118e0b9b
CaioLutzHonda <caiolutzhonda@gmail.com>
2022-03-26 18:36:46 -0400

    Primeira pagina feita faltando apenas a finalizacao

--- index.php
+++ index.php
@@ -3,12 +3,34 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/index.css">
 	<title>Cadastramento de Carro</title>
 </head>
 <body>
-	<form action="cadastrarCarro.php">
-		<button type="submit">Começar</button>
+	<div class="wrapper-tudo">
+		<div class="container-form">
+			<div class="form">
+				<h1 id="texto">Cadastre seu <br>carro com a gente.</h1>
+				<br>
+				<form action="cadastrarCarro.php" class="itens-form">
+					<button id="botao_form" type="submit">Começar</button>
+				</form>	
+			</div>
+		</div>	
+		<div class="lateral-image">
+			<div class="background">
+				<img src="images/Red-Car-Transparent-Free-PNG.png" id="imagem_carro" alt="imagem carro">
+			</div>
+		</div>	
+	</div>
+	<div class="bottom-bar">
+		<img class="social-icons" src="images/instagram-logo.svg" alt="instagram-logo">
+		<img class="social-icons" src="images/facebook-logo.svg" alt="facebook">
+	</div>
 		
-	</form>
+
+
+
+
 </body>
 </html>
